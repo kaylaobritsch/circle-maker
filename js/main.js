65133976fc95fea2cfe9fddddc8251940336ebde
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-26 16:27:35 -0400

    Ball not showing up

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ var $ball = $('.ball');
 
 $('#btn-spawn').on('keydown', function () {
   var $newBall = $('<div>');
+  var randLeft = Math.random();
+  var randTop = Math.random();
 
+  $newBall.addClass('ball');
+  $('body').append($newBall);
+  $('.ball').css('left', randLeft);
+  $('.ball').css('top', randTop);
 })
