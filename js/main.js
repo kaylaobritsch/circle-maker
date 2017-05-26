751c54a0356909e77db7c1309a5a7fb4f1c74bfd
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-26 16:48:51 -0400

    Spacing and indentation issues fixed

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ $('html').on('keydown', function () {
   var $newBall = $('<div>');
   var randLeft = Math.random() * 500;
   var randTop = Math.random() * 500;
-  var randColorList = ['royalblue', 'mistyrose', 'linen', 'lightcoral', 'palegreen']
+  var randColorList = ['royalblue', 'mistyrose', 'linen', 'lightcoral', 'palegreen'];
 
   $newBall.css('left', randLeft);
   $newBall.css('top', randTop);
-  $newBall.css('backgroundColor', randColorList[Math.floor(Math.random()*randColorList.length)]);
+  $newBall.css('backgroundColor', randColorList[Math.floor(Math.random() * randColorList.length)]);
   $newBall.addClass('ball');
   $('body').append($newBall);
-})
+});
